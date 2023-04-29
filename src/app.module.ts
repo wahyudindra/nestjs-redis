fbed16bd1de2e8c18c8671246cadf32d0091afbf
wahyudindra <wahyudi.indra@glovory.com>
2023-04-29 16:29:32 +0700

    [ADD] implement redis for cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { CacheModule } from '@nestjs/cache-manager';
 import { HttpModule } from '@nestjs/axios';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-    imports: [CacheModule.register({ isGlobal: true }), HttpModule.register({ timeout: 5000, maxRedirects: 5 })],
+    imports: [
+        // For nestjs cache
+        // CacheModule.register({ isGlobal: true }),
+
+        // For redis
+        CacheModule.register({
+            isGlobal: true,
+            store: redisStore,
+            host: 'localhost',
+            port: 6379,
+        }),
+        HttpModule.register({ timeout: 5000, maxRedirects: 5 }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
